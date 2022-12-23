92264e6b43043e871da1a5b52ffae02b6818a33b
NadyaGolub <nadyagolub@ukr.net>
2022-12-23 23:12:43 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,29 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(SimpleLightbox);
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(`.gallery`);
+const markupGalleryContainer = galleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML(`beforeend`, markupGalleryContainer);
+
+
+function galleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` 
+  <a class="gallery__item" href="${original}";>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join(``);
+}
+
+
+    let gallery = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250});
