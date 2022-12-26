26009f78d105c4208f5a1286a236385ab7b9d014
NadyaGolub <nadyagolub@ukr.net>
2022-12-26 20:41:33 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+
+const STORAGE_KEY = `feedback-form-state`;
+
+const formData = {};
+
+const refs = {
+    form: document.querySelector(`.feedback-form`),
+    textarea: document.querySelector(`.feedback-form textarea`),
+    }
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+refs.form.addEventListener(`input`, e => {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+
+    localStorage.setItem(`formData`, JSON.stringify(formData));
+    
+    if (localStorage.getItem(`STORAGE_KEY`)) {
+    formData = JSON.parse(localStorage.getItem(`STORAGE_KEY`));
+}
+})
+
+
+
+function onTextareaInput (evt) {
+    const massage = evt.target.value;
+
+    localStorage.setItem(STORAGE_KEY, massage);
+}
+
+
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea(evt) {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (saveMessage) {
+        console.log(saveMessage);
+        refs.textarea.value = saveMessage;
+    }
+}
